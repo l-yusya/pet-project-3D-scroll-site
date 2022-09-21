5ce9c9956a44b634d5ef199e396012595e0580a1
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-09-22 00:21:31 +0300

    Website created

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//3d scroll
+
+const zSpacing = -1000;
+let lastPos = zSpacing / 5;
+const $frames = document.getElementsByClassName('frame')
+const frames = Array.from($frames);
+let zVals = [];
+
+window.onscroll = function() {
+
+let top = document.documentElement.scrollTop;
+let delta = lastPos - top;
+lastPos = top;
+
+frames.forEach(function(n, i) {
+  zVals.push((i * zSpacing) + zSpacing);
+  zVals[i] += delta * -5.5;
+  let frame = frames[i];
+  let transform = `translateZ(${zVals[i]}px)`;
+  let opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+  frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+})
+
+}
+
+window.scrollTo(0, 1);
+
+//Audio
+
+const soundButton = document.querySelector('.soundbutton');
+const audio = document.querySelector('.audio');
+
+soundButton.addEventListener('click', e => {
+  soundButton.classList.toggle('paused');
+  audio.paused ? audio.play() : audio.pause();
+})
+
+window.onfocus = function () {
+  soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+}
+
+window.onblur = function () {
+  audio.pause();
+}
